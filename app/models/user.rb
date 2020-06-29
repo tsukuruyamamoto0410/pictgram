7805e44bd4ffc2af5fb353515d1771b1ac07a73f
”tsukuruyamamoto0410” <tukuru2410@i.softbank.jp>
2020-06-29 14:37:40 +0000

    fifth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  
+  validates :name, presence: true, length: {maximum: 15}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+ format: {with: VALID_EMAIL_REGEX}
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
+  validates :password, presence: true, length: {minimum: 8, maximum: 32},
+ format: {with: VALID_PASSWORD_REGEX}
+  
+  has_secure_password
 end
